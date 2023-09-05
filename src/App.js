79b3f696aaa5bee633ae24aae0535e70fa9acfd7
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-09-05 10:28:31 +0200

    Add grey color to main page

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import ScrollableAcceptanceBox from "./components/ScrollableAcceptanceBox";
 import {Box} from "@mui/material";
 
 function App() {
-  return (
-      <Box
-          sx={{
-            width: "100%",
-            height: "100dvh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-      >
-        <ScrollableAcceptanceBox></ScrollableAcceptanceBox>
-      </Box>
-  )
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                height: "100dvh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#a3ada6"
+            }}
+        >
+            <ScrollableAcceptanceBox></ScrollableAcceptanceBox>
+        </Box>
+    )
 }
-
 
 
 export default App;
