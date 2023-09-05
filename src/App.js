8fcafebf16079634c2a0dd38be591f0cdc600f78
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-09-05 10:11:02 +0200

    Add Box container with ScrollableAcceptanceBox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import ScrollableAcceptanceBox from "./components/ScrollableAcceptanceBox";
+import {Box} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Box
+          sx={{
+            width: "100%",
+            height: "100dvh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+      >
+        <ScrollableAcceptanceBox></ScrollableAcceptanceBox>
+      </Box>
+  )
 }
+
+
 
 export default App;
