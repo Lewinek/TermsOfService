4988c9426aef2458fac28571fb0bb987545e391c
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-09-19 12:14:09 +0200

    Make sessionReplayer as useRef instead of creating a new instance each time

--- src/components/ReplayPage.js
+++ src/components/ReplayPage.js
@@ -1,33 +1,40 @@
-import React, {useEffect} from 'react';
-import {Box, Typography} from "@mui/material";
-import {useLocation} from "react-router-dom";
+import React, { useEffect, useRef } from 'react';
+import { Box, Typography } from "@mui/material";
+import { useLocation } from "react-router-dom";
 
 function ReplayPage() {
     const location = useLocation();
     const events = location.state.events;
+    const sessionReplayer = useRef(null);
+
+    console.log('replay page');
 
     useEffect(() => {
-        if (events.length > 0) {
+        if (!sessionReplayer.current && events.length > 0) {
             // eslint-disable-next-line no-undef
-            const sessionReplayer  = new rrwebPlayer({
+            sessionReplayer.current = new rrwebPlayer({
                 target: document.body,
                 data: {
                     events,
                 }
             });
-            sessionReplayer.addEventListener('finish', () => {
-                console.log('finish')
+
+            sessionReplayer.current.addEventListener('finish', () => {
+                console.log('finish');
             });
 
             return () => {
-                sessionReplayer.removeEventListener('finish');
+                if (sessionReplayer.current) {
+                    sessionReplayer.current.pause();
+                    sessionReplayer.current = null;
+                }
             };
         }
-    }, [events]);
+    }, []);
 
     return (
         <Box>
-            <Typography variant="h5" sx={{mt: 1, ml: 2, fontWeight: `bold`}}>
+            <Typography variant="h5" sx={{ mt: 1, ml: 2, fontWeight: `bold` }}>
                 Replay
             </Typography>
         </Box>
