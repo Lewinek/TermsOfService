41d7e2e981127a388f598278150ddbe6b7ff47f7
Krzysztof Lewiński <krzysztof@coderspeak.com>
2023-09-19 10:11:01 +0200

    Add useEffect hookup to sessionReplayer

--- src/components/ReplayPage.js
+++ src/components/ReplayPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Box, Typography} from "@mui/material";
 import {useLocation} from "react-router-dom";
 
@@ -6,18 +6,30 @@ function ReplayPage() {
     const location = useLocation();
     const events = location.state.events;
 
-    //eslint-disable-next-line no-undef
-    const component = new rrwebPlayer({
-        target: document.body,
-        data: {
-            events,
-        },
-    });
-    component.addEventListener('finish', () => console.log('finish'));
+    useEffect(() => {
+        if (events.length > 0) {
+            // eslint-disable-next-line no-undef
+            const sessionReplayer  = new rrwebPlayer({
+                target: document.body,
+                data: {
+                    events,
+                }
+            });
+            sessionReplayer.addEventListener('finish', () => {
+                console.log('finish')
+            });
+
+            return () => {
+                sessionReplayer.removeEventListener('finish');
+            };
+        }
+    }, [events]);
 
     return (
         <Box>
-            <Typography variant="h5" sx={{mt: 1, ml: 2, fontWeight: `bold`}}>Replay</Typography>
+            <Typography variant="h5" sx={{mt: 1, ml: 2, fontWeight: `bold`}}>
+                Replay
+            </Typography>
         </Box>
     )
 }
